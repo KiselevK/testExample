07277281a4b5ae2d63c9cf9fb62188b2c8aff16c
Kyrylo Kyselov <kirill.kiselev220@gmail.com>
2025-10-02 22:50:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "<h1>Hello from Azure with Python! V1</h1>"
+    return "<h1>Hello from Azure with Python! V2</h1>"
 
 if __name__ == '__main__':
     app.run()
